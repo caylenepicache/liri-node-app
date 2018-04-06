7f79cd636cc2b51ccd6cd25213fcefd4990da8a2
unknown <caylenepicache@gmail.com>
2018-04-05 23:49:38 -0700

    keys

--- liri.js
+++ liri.js
@@ -1,13 +1,45 @@
 require("dotenv").config();
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var fs = require("fs");
+var request = require('request');
 
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
-var fs = require("fs");
-var myKey = ('./keys.js');
+
+var keys = ('./keys.js');
+var spotifyKey = new Spotify(keys.spotify);
+var clientTwitter = new Twitter(keys.twitter);
+
 
 console.log(process.argv[1]);
-var userInput = process.argv[2];
+var userInput = process.argv.slice(3).join('+');
 console.log(userInput);
 
+
+
+/*
+if (userInput === "spotify-this-song") {
+  if (!process.argv[3]) {
+    doSpotify();
+  }
+}
+
+function doSpotify() {
+
+  spotify.search({ type: 'track', query: 'The Sign', limit: 10 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      console.log("=============================================");
+      //artist name
+      console.log("The artist name is: " + data.tracks.items[5].artists[0].name)
+      //song name
+      console.log("The song name is: " + data.tracks.items[5].name)
+      //preview link
+      console.log("The preview link is: " + data.tracks.items[5].artists[0].href)
+      //album name
+      console.log("The album name is: " + data.tracks.items[5].album.name)
+      console.log("=============================================");
+    });
+  
+}; 
+*/
