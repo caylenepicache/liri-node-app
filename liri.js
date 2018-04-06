cc983b55e0118e7bda566ff19a0312be6300bdcd
unknown <caylenepicache@gmail.com>
2018-04-05 18:16:36 -0700

    for npm install

--- liri.js
+++ liri.js
@@ -1,1 +1,12 @@
-require("dotenv").config();
+// Includes the FS package for reading and writing packages
+var fs = require("dotenv").config();
+
+fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+  
+    console.log(data);
+    }
+  );
+  
