ff09e1bfcee5bce989ebf4f272f8ffd4423e022e
unknown <caylenepicache@gmail.com>
2018-04-05 22:39:13 -0700

    liri variables

--- liri.js
+++ liri.js
@@ -1,3 +1,13 @@
 require("dotenv").config();
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
-console.log(process.argv);
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var fs = require("fs");
+var myKey = ('./keys.js');
+
+console.log(process.argv[1]);
+var userInput = process.argv[2];
+console.log(userInput);
+
